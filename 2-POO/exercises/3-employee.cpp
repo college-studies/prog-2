615d3fd93ee32bdf4c19e8ed06efef90a5af1f24
Fillipe Augusto <fillipe484@gmail.com>
2021-03-17 08:24:27 -0300

    3-employees

--- 2-POO/exercises/3-employee.cpp
+++ 2-POO/exercises/3-employee.cpp
@@ -4,3 +4,42 @@ porcentagem) que aumente seu salario de acordo com a porcentagem passada no para
 mostraDados() que imprime na tela o nome e o salario do funcionario. Teste a classe no programa
 principal, mostrando os dados do funcionario antes e depois de um aumento concedido
 */
+#include <iostream>
+#include <string>
+using namespace std;
+class Empregado
+{
+public:
+  string nome;
+  float salario;
+
+  void aumento(float porcentagem)
+  {
+    salario = ((porcentagem / 100) * salario) + salario;
+  }
+
+  void mostraDados()
+  {
+    cout << "----------------\n"
+         << "Nome: " << nome << endl;
+    cout << "Salário: " << salario << endl;
+  }
+};
+
+int main()
+{
+
+  Empregado emp;
+
+  cout << "Insira aqui o nome do Empregado: ";
+  cin >> emp.nome;
+  cout << "Digite agora o salario: ";
+  cin >> emp.salario;
+
+  cout << "\n--------SALARIO ANTES---------"
+       << endl;
+  emp.mostraDados();
+  emp.aumento(10);
+  cout << "\n--------SALARIO COM AUMENTO---------" << endl;
+  emp.mostraDados();
+}
