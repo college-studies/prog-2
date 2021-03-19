c6be24e6404c01a88a1e2d919f46c9c17a382384
Fillipe Augusto <fillipe484@gmail.com>
2021-03-19 19:25:32 -0300

    exercicios poo

--- 2-POO/exercises/4-match.cpp
+++ 2-POO/exercises/4-match.cpp
@@ -6,36 +6,72 @@ visitor, int goalVisitor) que permita definir nome e numero de gols dos times; c
 calculaPontos() que define os pontos conquistados pelos times depois do resultado consumado.
 Crie tambem um metodo mostraResultado() que imprima na tela o resultado final como no
 formato Visitante 4 x 2 Mandante. Instancie dois objetos desta classe e teste seus metodos no
-programa principal
+programa principal....
 */
 #include <iostream>
 #include <string>
+
 using namespace std;
 class Partida
 {
 public:
-  string home_Name;
-  int goalHome;
-  int pontCasa;
+  string nameHome;
+  int goals_home;
+  int points_home;
 
-  string visitor_Name;
-  int goalVisitor;
-  int pontVisitante;
+  string nameVisitor;
+  int goals_visitor;
+  int points_visitor;
 
   void resultado(string home, int goalHome, string visitor, int goalVisitor)
   {
+    nameHome = home;
+    goals_home = goalHome;
+    nameVisitor = visitor;
+    goals_visitor = goalVisitor;
   }
 
-  void calculaPontos() {}
-  void mostraResultado() {}
+  void calculaPontos()
+  {
+
+    if (goals_home > goals_visitor)
+    {
+      points_home = 3;
+      points_visitor = 0;
+    }
+    else if (goals_home < goals_visitor)
+    {
+      points_visitor = 3;
+      points_home = 0;
+    }
+    else
+    {
+      points_home = 1;
+      points_visitor = 1;
+    }
+  }
+
+  void mostraResultado()
+  {
+    cout << nameVisitor << " " << goals_visitor << " x " << goals_home << " " << nameHome << endl;
+  }
 };
 
 int main()
 {
-  Partida home, visitor;
+  Partida team1, team2;
 
-  cout << "Nome time de casa: ";
-  cin >> home.home_Name;
-  cout << "\nNome time visitante: ";
-  cin >> visitor.visitor_Name;
+  cout << "Nome de Casa: ";
+  cin >> team1.nameHome;
+  cout << "Gols: ";
+  cin >> team1.goals_home;
+
+  cout << "\nNome Visitante: ";
+  cin >> team2.nameVisitor;
+  cout << "Gols: ";
+  cin >> team2.goals_visitor;
+
+  team1.resultado(team1.nameHome, team1.goals_home, team2.nameVisitor, team2.goals_visitor);
+  team1.calculaPontos();
+  team1.mostraResultado();
 }
